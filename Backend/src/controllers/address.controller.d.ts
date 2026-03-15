54d122e4a0618e64e989783d3c956656892caa44
Ayush Amoli <ayushamoli63@gmail.com>
2026-03-15 23:48:34 +0530

    Comprehensive cleanup of compiled files in Backend to fix Vercel entrypoint conflict

--- Backend/src/controllers/address.controller.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Response } from 'express';
-export declare const getAddresses: (req: any, res: Response) => Promise<void>;
-export declare const createAddress: (req: any, res: Response) => Promise<void>;
-export declare const deleteAddress: (req: any, res: Response) => Promise<void>;
-//# sourceMappingURL=address.controller.d.ts.map
